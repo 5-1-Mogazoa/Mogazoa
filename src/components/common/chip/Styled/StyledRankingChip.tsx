5432ef2cfcc5dbd1145254c6d97b822fc8051054
yspark <ys1008p@gmail.com>
2024-03-02 22:30:37 +0900

    refactor: ranking chip 3위 이하 타입 통합

--- src/components/common/chip/Styled/StyledRankingChip.tsx
+++ src/components/common/chip/Styled/StyledRankingChip.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { fontStyle } from "@/styles/theme";
 
 type StyledRankingChipProps = {
-  $ranking: "1등" | "2등" | "3등";
+  $ranking: "1등" | "2등" | "others";
 };
 
 export const StyledRankingChip = styled.div<StyledRankingChipProps>`
@@ -24,7 +24,7 @@ export const StyledRankingChip = styled.div<StyledRankingChipProps>`
         return "var(--color-pink-10, rgba(255, 47, 159, 0.10))";
       case "2등":
         return "var(--color-green-10, rgba(5, 213, 139, 0.10))";
-      case "3등":
+      case "others":
         return "rgba(159, 166, 178, 0.10)";
     }
   }};
@@ -35,7 +35,7 @@ export const StyledRankingChip = styled.div<StyledRankingChipProps>`
         return "var(--color-pink-ff, #FF2F9F)";
       case "2등":
         return "var(--color-green-ff, #05D58B)";
-      case "3등":
+      case "others":
         return "var(--color-gray-9f, #9FA6B2)";
     }
   }};
