f64856fe1624ceb8189e3c6b3766d866b3a21e83
yspark <ys1008p@gmail.com>
2024-03-04 21:59:35 +0900

    feat: SignUpForm의 비밀번호, 비밀번호 확인, 가입하기 버튼 구현

--- src/components/auth/signUp/SignUpForm.tsx
+++ src/components/auth/signUp/SignUpForm.tsx
@@ -1,13 +1,54 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import {
   StyledDescription,
   StyledInput,
   StyledInputContainer,
   StyledLabel,
+  StyledPasswordInputContainer,
+  StyledPasswordOnOffButton,
 } from "../../common/input/Styled/StyledInput";
 import ERROR_MESSAGE from "@/src/constant/ERROR_MESSAGE";
 import REGEX from "@/src/constant/REGEX";
+import { StyledPrimaryButton } from "../../common/button/Styled/StyledButton";
+import styled from "styled-components";
+
+const StyledSignUpForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 335px;
+  gap: 30px;
+  padding-top: 30px;
+  margin: 0 auto;
+
+  @media (min-width: ${({ theme }) => theme.deviceSizes.tablet}) {
+    padding-top: 180px;
+    width: 440px;
+    gap: 40px;
+  }
+
+  @media (min-width: ${({ theme }) => theme.deviceSizes.desktop}) {
+    padding-top: 90px;
+    width: 640px;
+  }
+`;
+
+const StyledSignUpButtonContainer = styled.div`
+  margin-top: 126px;
+  width: 335px;
+  height: 50px;
+
+  @media (min-width: ${({ theme }) => theme.deviceSizes.tablet}) {
+    margin-top: 20px;
+    width: 440px;
+    height: 55px;
+  }
+
+  @media (min-width: ${({ theme }) => theme.deviceSizes.desktop}) {
+    width: 640px;
+    height: 65px;
+  }
+`;
 
 type SignUpData = {
   email: String;
@@ -22,16 +63,27 @@ export default function SignUpForm() {
     handleSubmit,
     setError,
     formState: { errors },
-  } = useForm<SignUpData>();
-  const onSubmit = (data: SignUpData) => console.log(data);
-  console.log(errors);
+  } = useForm<SignUpData>({ mode: "onBlur" });
+  const onSubmit = (data: SignUpData) => {
+    if (data.password !== data.passwordConfirmation) {
+      setError("passwordConfirmation", { type: "matched", message: ERROR_MESSAGE.NOT_MATCH_PASSWORD });
+      return;
+    }
+    console.log(data);
+  };
+  const hasError = Object.values(errors).length;
+
+  const [isPWView, setIsPWView] = useState(false);
+  const [isPWConfirmationView, setIsPWConfirmationView] = useState(false);
+  const handlePWView = () => setIsPWView(!isPWView);
+  const handlePWConfirmationView = () => setIsPWConfirmationView(!isPWConfirmationView);
 
   // TODO : email, nickname 중복시 사용할 에러추가 코드 추가 예정
   // setError("email", { type: "duplicated", message: ERROR_MESSAGE.DUPLICATE_EMAIL });
   // setError("nickname", { type: "duplicated", message: ERROR_MESSAGE.DUPLICATE_NICKNAME });
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <StyledSignUpForm onSubmit={handleSubmit(onSubmit)}>
       <StyledInputContainer>
         <StyledLabel htmlFor="signUpEmailInput">이메일</StyledLabel>
         <StyledInput
@@ -72,9 +124,58 @@ export default function SignUpForm() {
         />
         {errors.nickname && <StyledDescription $isError>{errors.nickname.message} </StyledDescription>}
       </StyledInputContainer>
-
+      <StyledInputContainer>
+        <StyledLabel htmlFor="signUpPassword">비밀번호</StyledLabel>
+        <StyledPasswordInputContainer>
+          <StyledInput
+            $isError={errors.password ? true : false}
+            type={isPWView ? "text" : "password"}
+            placeholder="비밀번호를 입력해 주세요"
+            {...register("password", {
+              required: {
+                value: true,
+                message: ERROR_MESSAGE.REQUIRED_PASSWORD,
+              },
+              pattern: {
+                value: REGEX.PASSWORD,
+                message: ERROR_MESSAGE.REQUIRED_PASSWORD_FORMAT,
+              },
+              minLength: {
+                value: 8,
+                message: ERROR_MESSAGE.PASSWORD_MIN_LENGTH,
+              },
+            })}
+            id="signUpPassword"
+          />
+          <StyledPasswordOnOffButton onClick={handlePWView} $isVisibility={isPWView} />
+        </StyledPasswordInputContainer>
+        {errors.password && <StyledDescription $isError>{errors.password.message} </StyledDescription>}
+      </StyledInputContainer>
+      <StyledInputContainer>
+        <StyledLabel htmlFor="signUpPasswordConfirmation">비밀번호 확인</StyledLabel>
+        <StyledPasswordInputContainer>
+          <StyledInput
+            $isError={errors.passwordConfirmation ? true : false}
+            type={isPWConfirmationView ? "text" : "password"}
+            placeholder="비밀번호를 한번 더 입력해 주세요"
+            {...register("passwordConfirmation", {
+              required: {
+                value: true,
+                message: ERROR_MESSAGE.REQUIRED_PW_COMFIRMATION,
+              },
+            })}
+            id="signUpPasswordConfirmation"
+          />
+          <StyledPasswordOnOffButton onClick={handlePWConfirmationView} $isVisibility={isPWConfirmationView} />
+        </StyledPasswordInputContainer>
+        {errors.passwordConfirmation && (
+          <StyledDescription $isError>{errors.passwordConfirmation.message} </StyledDescription>
+        )}
+      </StyledInputContainer>
       {/* TODO: 제출 버튼 추가 예정 */}
-      <input type="submit" />
-    </form>
+      <StyledSignUpButtonContainer>
+        <StyledPrimaryButton disabled={hasError ? true : false}>가입하기</StyledPrimaryButton>
+      </StyledSignUpButtonContainer>
+    </StyledSignUpForm>
   );
 }
