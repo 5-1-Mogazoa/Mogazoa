aac7009e676e1edc2e3f32e81b7a1ff555a8054b
yspark <ys1008p@gmail.com>
2024-03-04 14:04:48 +0900

    feat: ERROR_MESSAGE 상수 설정

---
+++ src/constant/ERROR_MESSAGE.ts
@@ -0,0 +1,15 @@
+const ERROR_MESSAGE = {
+  REQUIRED_EMAIL: "이메일은 필수 입력입니다.",
+  REQUIRED_EMAIL_FORMAT: "이메일 형식으로 작성해 주세요.",
+  DUPLICATE_EMAIL: "이미 사용중인 이메일입니다.",
+  REQUIRED_NICKNAME: "닉네임은 필수 입력입니다.",
+  NICKNAME_MAX_LENGTH: "닉네임은 최대 20자까지 가능합니다",
+  DUPLICATE_NICKNAME: "이미 사용중인 닉네임입니다.",
+  REQUIRED_PASSWORD: "비밀번호는 필수 입력입니다.",
+  REQUIRED_PASSWORD_FORMAT: "비밀번호는 숫자, 영문, 특수문자로만 가능합니다.",
+  PASSWORD_MIN_LENGTH: "비밀번호는 최소 8자 이상입니다.",
+  REQUIRED_PW_COMFIRMATION: "비밀번호 확인을 입력해주세요.",
+  NOT_MATCH_PASSWORD: "비밀번호가 일치하지 않습니다.",
+};
+
+export default ERROR_MESSAGE;
