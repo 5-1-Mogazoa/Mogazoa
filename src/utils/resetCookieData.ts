06412a6feea6b2ca0e16bb2a5af63801e407c362
yspark <ys1008p@gmail.com>
2024-03-05 17:57:06 +0900

    refactor: accessToken, userId 저장 cookie에서 localstorage 사용으로 변경

--- src/utils/resetCookieData.ts
+++
@@ -1,6 +0,0 @@
-const resetCookieData = () => {
-  document.cookie = "accessToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; secure; SameSite=None;";
-  document.cookie = "userId=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; secure; SameSite=None;";
-};
-
-export default resetCookieData;
