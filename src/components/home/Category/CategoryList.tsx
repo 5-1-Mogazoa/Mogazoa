53b7d069921baec2385f388061d5277729c6e7be
youngeun <00eun1128@gmail.com>
2024-03-11 15:58:01 +0900

    fix : 카테고리 한번더 클릭시 선택해제 기능 추가

--- src/components/home/Category/CategoryList.tsx
+++ src/components/home/Category/CategoryList.tsx
@@ -1,23 +1,32 @@
-import { useState } from "react";
-import { useRouter } from "next/router";
 import { useQuery } from "@tanstack/react-query";
 import { getCategoryList } from "@/src/apis/category";
 import Category from "./category";
 import * as S from "./Styled/StyledCategoryList";
-import useFilterSearch, { filterSearchProps } from "../../search/useFilterSearch";
+import useFilterSearch from "../../search/useFilterSearch";
+import { useState } from "react";
 
 export default function CategoryList() {
   const filterSearch = useFilterSearch();
+  const [isSelected, setIsSelected] = useState<string | undefined>("");
   const { data: categoryList } = useQuery({
     queryKey: ["categoryList"],
     queryFn: () => getCategoryList(),
   });
+
   if (!categoryList) {
     return null;
   }
 
   const onChange = (e) => {
-    filterSearch({ category: e.target.value });
+    const selectedValue = e.target.value;
+
+    // 이미 선택된 값과 같은 경우에만 해제
+    if (isSelected === selectedValue) {
+      setIsSelected(undefined);
+    } else {
+      setIsSelected(selectedValue);
+      filterSearch({ category: e.target.value });
+    }
   };
 
   return (
@@ -26,7 +35,14 @@ export default function CategoryList() {
         <S.CategoryTitle>카테고리</S.CategoryTitle>
         <S.CategoryListBox>
           {categoryList?.map((item: any, index: number) => (
-            <Category id={item.id} value={item.name} label={item.name} key={index} onChange={onChange} />
+            <Category
+              id={item.id}
+              value={item.name}
+              label={item.name}
+              key={index}
+              onChange={onChange}
+              checked={isSelected === item.name}
+            />
           ))}
         </S.CategoryListBox>
       </S.CategoryListWrap>
--- src/components/home/Category/category.tsx
+++ src/components/home/Category/category.tsx
@@ -1,18 +1,37 @@
 import { useQuery } from "@tanstack/react-query";
 import { getCategoryList } from "@/src/apis/category";
 import * as S from "./Styled/StyledCategory";
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 type CategoryProps = {
   id: number;
   value: string;
   label: string;
+  checked?: boolean;
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 };
-export default function Category({ id, value, onChange, label }: CategoryProps) {
+
+export default function Category({ id, value, onChange, label, checked }: CategoryProps) {
+  const radioRef = useRef(null);
+
+  const handleClick = (e) => {
+    if (e.target === radioRef.current && radioRef.current.checked) {
+      e.preventDefault();
+      onChange({ target: { value: "" } });
+    }
+  };
+
   return (
-    <S.RadioLabel>
-      <S.HiddenRadioInput type="radio" id={id} name="categoryGroup" value={value} onChange={onChange} />
+    <S.RadioLabel onClick={handleClick}>
+      <S.HiddenRadioInput
+        type="radio"
+        id={String(id)}
+        name="categoryGroup"
+        value={value}
+        onChange={onChange}
+        checked={checked}
+        ref={radioRef}
+      />
       <S.CustomRadio>{label}</S.CustomRadio>
     </S.RadioLabel>
   );
