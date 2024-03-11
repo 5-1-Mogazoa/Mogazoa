ae083eaaffd6f94649e18c618b2c23805148f466
youngeun <00eun1128@gmail.com>
2024-03-11 16:48:55 +0900

    Design : 마진 및 레이아웃 UI 구현

--- src/components/search/SearchProducts/SearchProducts.tsx
+++ src/components/search/SearchProducts/SearchProducts.tsx
@@ -1,14 +1,18 @@
 import { useState, useEffect } from "react";
-import SortDropDown from "../../common/button/SortDropdown";
-import SearchCardList from "../SearchCardList/SearchCardList";
-import SearchTitle from "../SearchTitle/SearchTitle";
 import { useRouter } from "next/router";
 import { OrderType } from "@/pages/products/[productId]";
 import useFilterSearch, { filterSearchProps } from "../useFilterSearch";
+import SortDropDown from "../../common/button/SortDropdown";
+import SearchCardList from "../SearchCardList/SearchCardList";
+import SearchTitle from "../SearchTitle/SearchTitle";
+import CategoryDropDown from "../CategoryDropDown/CategoryDropDown";
+import { SearchProductsBox, SearchFilterBox } from "./Styled/StyledSearchProduct";
 
 export default function SearchProducts() {
   const [order, setOrder] = useState<OrderType>({ id: "recent", name: "최신순" });
   const filterSearch = useFilterSearch();
+  const [searchKeyword, setSearchKeyword] = useState<string>("");
+  const [dynamicMargin, setDynamicMargin] = useState<boolean>(false);
 
   const handleSortButtonClick = (orderItem: OrderType) => {
     const orderId = orderItem.id;
@@ -19,23 +23,35 @@ export default function SearchProducts() {
 
   const router = useRouter();
   const { searchQuery, category, sortValue } = router.query;
-  const [searchKeyword, setSearchKeyword] = useState("");
+
+  const resultJSX = (
+    <div>
+      {`${category} 카테고리의`} <br />
+      {`${searchQuery}로 검색한 상품`}
+    </div>
+  );
 
   useEffect(() => {
     if (searchQuery && category) {
-      setSearchKeyword(`${category} 카테고리의 ${searchQuery}로 검색한 상품`);
+      setSearchKeyword(resultJSX);
+      setDynamicMargin(true);
     } else if (category) {
       setSearchKeyword(`${category}의 모든 상품`);
-    } else if (searchQuery && category) {
+      setDynamicMargin(false);
+    } else if (searchQuery) {
       setSearchKeyword(`${searchQuery}로 검색한 상품`);
+      setDynamicMargin(false);
     }
   }, [searchQuery, category]);
 
   return (
-    <>
+    <SearchProductsBox>
       <SearchTitle searchKeyword={searchKeyword} />
-      <SortDropDown type="home" selectedItem={order} handleOrderButtonClick={handleSortButtonClick} />
+      <SearchFilterBox>
+        <CategoryDropDown />
+        <SortDropDown type="home" selectedItem={order} handleOrderButtonClick={handleSortButtonClick} />
+      </SearchFilterBox>
       <SearchCardList order={order} category={category} searchQuery={searchQuery} />
-    </>
+    </SearchProductsBox>
   );
 }
--- src/components/search/SearchProducts/Styled/StyledSearchProduct.tsx
+++ src/components/search/SearchProducts/Styled/StyledSearchProduct.tsx
@@ -1,2 +1,25 @@
 import { fontStyle } from "@/styles/theme";
 import styled from "styled-components";
+const SearchProductsBox = styled.div`
+  padding: 0 20px;
+
+  @media (min-width: ${({ theme }) => theme.deviceSizes.tablet}) {
+    padding: 0 30px;
+  }
+
+  @media (min-width: ${({ theme }) => theme.deviceSizes.desktop}) {
+    padding: 0;
+  }
+`;
+const SearchFilterBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 15px;
+
+  @media (min-width: ${({ theme }) => theme.deviceSizes.tablet}) {
+    justify-content: flex-end;
+  }
+`;
+
+export { SearchProductsBox, SearchFilterBox };
