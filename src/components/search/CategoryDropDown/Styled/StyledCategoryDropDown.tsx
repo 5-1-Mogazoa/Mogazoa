8732d46dd428d4a205a20b3d84e18d511fed0e7d
youngeun <00eun1128@gmail.com>
2024-03-11 15:59:52 +0900

    feat : 모바일사이즈 카테고리 필터 UI 구현

---
+++ src/components/search/CategoryDropDown/Styled/StyledCategoryDropDown.tsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+import { fontStyle } from "@/styles/theme";
+
+const CategoryFilter = styled.div`
+  display: flex;
+  padding: 6px 12px;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+
+  border-radius: 100px;
+  border: 1px solid var(--color-black-35, #353542);
+  background: var(--color-black-25, #252530);
+  @media (min-width: ${({ theme }) => theme.deviceSizes.tablet}) {
+    display: none;
+  }
+`;
+
+const FilterText = styled.div`
+  color: var(--color-gray-9f, #9fa6b2);
+
+  ${fontStyle({ w: 400, s: 14, l: 17 })};
+`;
+export { CategoryFilter, FilterText };
