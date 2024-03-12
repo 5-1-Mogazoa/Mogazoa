1254d4d6c2a2fe3033ed902c1583843730080c26
youngeun <00eun1128@gmail.com>
2024-03-12 11:21:25 +0900

    fix : 모바일일때 타이틀 구현 안되는 오류 수정

--- src/components/search/SearchTitle/SearchTitle.tsx
+++ src/components/search/SearchTitle/SearchTitle.tsx
@@ -1,9 +1,31 @@
 import { SearchTitleName } from "./Styled/StyledSearchTitle";
+import { useState, useEffect } from "react";
+
 type SearchTitleProps = {
-  searchKeyword: string;
+  searchQuery?: string | string[] | undefined;
+  category?: string | string[] | undefined;
 };
 
-export default function SearchTitle({ searchKeyword }: SearchTitleProps) {
+export default function SearchTitle({ searchQuery, category }: SearchTitleProps) {
+  const [searchKeyword, setSearchKeyword] = useState<string>("");
+  const resultJSX = (
+    <div>
+      {`${category} 카테고리의`} <br />
+      {`${searchQuery}로 검색한 상품`}
+    </div>
+  );
+
+  useEffect(() => {
+    console.log(category);
+    if (searchQuery && category) {
+      setSearchKeyword(resultJSX);
+    } else if (category) {
+      setSearchKeyword(`${category}의 모든 상품`);
+    } else if (searchQuery) {
+      setSearchKeyword(`${searchQuery}로 검색한 상품`);
+    }
+  }, [searchQuery, category]);
+
   return (
     <div>
       <SearchTitleName>{searchKeyword}</SearchTitleName>
