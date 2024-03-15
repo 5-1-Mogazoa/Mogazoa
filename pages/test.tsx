9f316e8eb1af31f67bfb6543699c421c67c6c30a
yspark <ys1008p@gmail.com>
2024-03-15 22:24:06 +0900

    feat:마이 페이지 프로필 편집, 로그아웃 버튼 구현

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,9 +1,5 @@
-import FollowInfoModal from "@/src/components/profiles/FollowInfoModal";
-import ProfileEditModal from "@/src/components/profiles/ProfileEditModal";
-import { useEffect, useState } from "react";
+import MyPageProfileButtons from "@/src/components/profiles/MyPageProfileButtons";
 
-export default function Test() {
-  const [isOpen, setIsOpen] = useState(true);
-  // return isOpen && <FollowInfoModal nickname="test" userId={17} dataType="follower" setIsOpen={setIsOpen} />;
-  return isOpen && <ProfileEditModal setIsOpen={setIsOpen}></ProfileEditModal>;
+export default function Test2() {
+  return <MyPageProfileButtons />;
 }
---
+++ src/components/profiles/MyPageProfileButtons.tsx
@@ -0,0 +1,29 @@
+import { useRouter } from "next/router";
+import { StyledButtonsContainer, StyledLogOutButton, StyledProfileEditButton } from "./Styled/StyledMyPageButton";
+import { resetToken } from "@/src/apis/auth";
+import ProfileEditModal from "./ProfileEditModal";
+import { useState } from "react";
+
+export default function MyPageProfileButtons() {
+  const router = useRouter();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleProfileEditButton = () => {
+    setIsOpen(true);
+  };
+
+  const handleLogOutButton = () => {
+    //TODO: localStorage 설정 차후 제거
+    localStorage.clear();
+    resetToken();
+    router.push("/");
+  };
+
+  return (
+    <StyledButtonsContainer>
+      <StyledProfileEditButton onClick={handleProfileEditButton}>프로필 편집</StyledProfileEditButton>
+      {isOpen && <ProfileEditModal setIsOpen={setIsOpen} />}
+      <StyledLogOutButton onClick={handleLogOutButton}>로그아웃</StyledLogOutButton>
+    </StyledButtonsContainer>
+  );
+}
