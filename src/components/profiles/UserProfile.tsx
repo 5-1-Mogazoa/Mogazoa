9b14a2bdb181d378c55a17cb47f6e1e66631eafa
muyahho <hoodysonyeo@gmail.com>
2024-03-27 11:41:51 +0900

    파일 프로덕트  타입오류 수정

--- src/components/profiles/UserProfile.tsx
+++ src/components/profiles/UserProfile.tsx
@@ -183,18 +183,6 @@ const StyledFollowText = styled.div`
   }
 `;
 
-// const StyledFloatButton = styled.button`
-//   position: fixed;
-//   bottom: 40px;
-//   right: 40px;
-//   height: 60px;
-//   width: 60px;
-//   border-radius: 50%;
-//   font-size: 36px;
-//   color: white;
-//   background: var(--main-main_gradation, linear-gradient(91deg, #5097fa 0%, #5363ff 100%));
-// `;
-
 type Props = {
   isMe: boolean;
 };
